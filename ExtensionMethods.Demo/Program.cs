2d6921995b6ce994549f20e6b28d2fbd2ff62e8a
md.rubel <md.rubel@HOAP2406101764A>
2021-12-04 16:53:35 +0600

    Add project files.

---
+++ ExtensionMethods.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var items = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var items = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            //var evenNumbers = items.Where(x => x % 2 == 0);
+            var evenNumbers = items.Filter(x=>x>7).Select(x => $"Item Number: {x}");
+            Console.WriteLine($"Even Number: {string.Join(',', evenNumbers)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethods.Demo/StringArrayExtention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods.Demo
+{
+    public static class StringArrayExtention
+    {
+        public static IEnumerable<int> Filter(this IEnumerable<string> items,Func<int,bool> func)
+        {
+            return items.Select(x => Convert.ToInt32(x)).Where(x=>func(x));
+        }
+    }
+}
